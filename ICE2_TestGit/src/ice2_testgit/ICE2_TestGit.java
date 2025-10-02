4795d5166479fdc856f546f86b81bd5e00926097
Miko26-cyber <manuelm@sheridancollege.ca>
2025-10-02 09:44:07 -0400

    rev 1 local

---
+++ ICE2_TestGit/src/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ice2_testgit;
+
+/**
+ *
+ * @author mikom
+ */
+public class ICE2_TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
